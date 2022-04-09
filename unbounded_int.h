a6ba7ab285a4b8aaf37d888dc9f8284afb24bcc5
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2022-04-09 22:08:44 +0200

    fonctions dans le fichier .h

--- unbounded_int.h
+++ unbounded_int.h
@@ -25,10 +25,17 @@ unbounded_int string2unbounded_int(const char *e);
 
 unbounded_int ll2unbounded_int(long long i);
 
+char *unbounded_int2string(unbounded_int i);
+
+int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b);
+
+int unbounded_int_cmp_ll(unbounded_int a, long long b);
+
+unbounded_int unbounded_int_somme(unbounded_int a, unbounded_int b);
+
+unbounded_int unbounded_int_difference( unbounded_int a, unbounded_int b);
+
+unbounded_int unbounded_int_produit( unbounded_int a, unbounded_int b)
 
 
-int main(void) {
 
-
-    return 0;
-}
