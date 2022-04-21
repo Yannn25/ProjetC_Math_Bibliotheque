a4abcf0d99851b9aa945b82e9c2f8c4d676c56e0
Yann EDIKEU <yann.edikeu@etu.u-paris.fr>
2022-04-21 16:00:23 +0200

    V2 de la méthode fini avec test

--- test_unbounded.c
+++ test_unbounded.c
@@ -3,5 +3,14 @@
 #include "unbounded_int.c"
 
 int main(void) {
-    return 0;
+    char *nb = "235";
+    unbounded_int n = string2unbounded_int(nb);
+
+    affiche_unbounded_int(n);
+
+    char *t = "test du testeur";
+    printf("\n ------------ \n%s \n ----------- \n",t);
+    char *unb = unbounded_int2string(n);
+    printf("\n ------------ \n%s \n ----------- \n",unb);
+
 }
