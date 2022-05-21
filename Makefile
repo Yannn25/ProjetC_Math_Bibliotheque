897391eda8fc272917d7f37ce92a2141ce79c559
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2022-05-21 16:48:04 +0200

    rebase

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@ CC = gcc
 
 #CFLAGS = -Wall -g -pedantic
 
-prog :unbounded_int.o test_unbounded.o 
-	$(CC) -Wall -o prog unbounded_int.o test_unbounded.o
+prog : unbounded_int.o test_unbounded.o 
+	$(CC) -Wall -g -o prog unbounded_int.o test_unbounded.o
 
 unbounded_int.o : unbounded_int.c
-	$(CC) -Wall -o unbounded_int.o -c unbounded_int.c
+	$(CC) -Wall -g  -o unbounded_int.o -c unbounded_int.c
 test_unbounded.o : test_unbounded.c
-	$(CC) -Wall -o test_unbounded.o -c test_unbounded.c
+	$(CC) -Wall -g -o test_unbounded.o -c test_unbounded.c
 clean :
 	rm -rf *.o && rm -rf prog
--- test_unbounded.c
+++ test_unbounded.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-#include "unbounded_int.c"
+#include "unbounded_int.h"
+static void affiche_unbounded_int(unbounded_int a);
 
 int main(void) {
     printf("\n ------ STRING TO UNBOUNDED INT ------\n");
@@ -74,4 +75,14 @@ int main(void) {
     r = ll2unbounded_int(20);
     sous = unbounded_int_difference(q,r);
     affiche_unbounded_int(sous);
+}
+
+static void affiche_unbounded_int(unbounded_int a) {
+    printf("Longueur : %ld et Signe : %c\nunbounded_int : ",a.len,a.signe);
+    chiffre *tmp = a.premier;
+    while( tmp !=  NULL){
+        printf("%c ",tmp->c);
+        tmp = tmp->suivant;
+    }
+    printf("\n");
 }
