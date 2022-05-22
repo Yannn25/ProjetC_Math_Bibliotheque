3c9b69ea7c3d7fe81b18a467d4cb168ab916e82f
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2022-05-22 19:42:51 +0200

    condi

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -9,47 +9,58 @@
     lorsqu'on va lire le fichier :
     pour chaque LIGNE :
     stocker le mot jusqu'a voir un espace
-    si le mot est un chiffre on le transforme en unbounded int (avec son signe)
-        si on rencontre une opération on la stock dans une variable
-    on recupére le deuxieme nombre comme le 1
-    on traite les deux unbounded int
 
     si le mot est juste une variable(juste un char)
     on cherche si la vraiable existe dans notre liste de variable (si elle n'xiste pas on la créera avec les infos qui sont après le =)
     on fait les opération demandés sur cette variable
+
+    si le mot est un chiffre on le transforme en unbounded int (avec son signe)
+        si on rencontre une opération on la stock dans une variable
+    on recupére le deuxieme nombre comme le 1
+    on traite les deux unbounded int
 
     si le mot est une instruction
      on stock l'instruction (print)
         et on l'execute sur la variable qui se situe après si la variable n'existe pas print 0
 */
 
-typedef struct 
+typedef struct variable
 {
     long value;
     char name;
     unbounded_int ub;
-    variable *next;
+    struct variable *next;
 }variable;
 
-typedef struct 
+typedef struct Liste
 {
     variable *first;
 }Liste;
 
-int find(char variable){
+int find(Liste l,char variable){
     return 0;
 }
 
-void ajoutVariable(Liste l,variable v){//ajoute une variable a la in de la liste    
+/*variable createVariable(char val,char nom){
+
+}*/
+
+void ajoutVariable(Liste *l,variable *v){//ajoute une variable au début de la liste    
+        /* Création du nouvel élément */
+  
+
+    /* Insertion de l'élément au début de la liste */
+    v->next = l->first;
+    l->first = v;
 
 }
 
-void clear(Liste l){ //vide la liste
+void clear(Liste *liste){ //vide la liste
     if (liste == NULL)
     {
         exit(EXIT_FAILURE);
     }
-    if (liste->premier != NULL)
+    if (liste->first != NULL)
     {
         variable *aSupprimer = liste->first;
         liste->first = liste->first->next;
@@ -59,40 +70,47 @@ void clear(Liste l){ //vide la liste
 
 int main(int argc, char const *argv[])
 {
-    FILE *src;
-    FILE *dst;
+    FILE *src = malloc(sizeof(FILE));
+    FILE *dst = malloc(sizeof(FILE));
 
-    
+    char ligne [1024];
 
-     if(len(argv) == 3){
-        src = fopen (argv [1],"r");
+     if(argc == 3){
+        src = fopen (argv [1],"r+");
         dst = fopen (argv [2],"a+");
     }
 
-    if (len(argv) == 4)
+    if (argc == 4)
     {
-        if (strcmp("-i",argv[1]))
+        if ("-i" == argv[1])
         {
             printf("que l'option -i");
+            src = fopen (argv [2],"r+");
+            dst = stdout;
         }
 
-        if (strcmp("-o",argv[2]))
+        if ("-o" == argv[2])
         {
-             printf("que l'option -o");
+            printf("que l'option -o");
+            src = stdin;
+            dst = fopen (argv [3],"a+");
         }
         
         
     }
 
-    if (len(argv)==5)
+    if (argc == 5)
     {
-        
+         src = fopen (argv [2],"r+");
+         dst = fopen (argv [4],"a+");
     }
     
-     while (fgets(texte,1024,src) != NULL)
+     while (fgets(ligne,255,src) != NULL)
     {
-        printf("%s",texte);
+        printf("%s",ligne);
     }
 
+    fclose(src);
+    fclose(dst);
     return 0;
 }
